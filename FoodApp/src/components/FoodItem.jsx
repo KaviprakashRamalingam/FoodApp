188aa80b1f796d593a819877aa4aab24eb0ce393
KaviprakashRamalingam <ramalingam.ka@northeastern.edu>
2024-06-13 10:35:05 -0400

    Add Food Item modules

--- FoodApp/src/components/FoodItem.jsx
+++ FoodApp/src/components/FoodItem.jsx
@@ -1,9 +1,21 @@
-export default function FoodItem({ food }) {
+import styles from "./foodItem.module.css";
+export default function FoodItem({ food, setFoodId }) {
   return (
-    <div>
-      <img src={food.image} alt="" />
-      <h1>{food.title}</h1>
-      <button>View Reciepe</button>
+    <div className={styles.itemContainer}>
+      <img className={styles.image} src={food.image} alt="" />
+      <div className={styles.content}>
+        <p className={styles.itemName}>{food.title}</p>
+      </div>
+      <div className={styles.buttonContainer}>
+        <button
+          onClick={() => {
+            setFoodId(food.id);
+          }}
+          className={styles.itemButton}
+        >
+          View Reciepe
+        </button>
+      </div>
     </div>
   );
 }
